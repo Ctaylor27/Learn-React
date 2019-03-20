e7988b6e076772dc366c379259c3692b0561c62d
Caleb Taylor <cteuphonium@gmail.com>
2019-03-20 17:39:00 -0500

    This project is being used with a Udemy course to learn React.  In this commit, I learned about using create-react-app, the files it generates, components, was introduced to the concept of props, and was introduced to state.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Caleb', age: '22'},
+      { name: 'Stefani', age: '21'},
+      { name: 'Chester', age: '4'},
+    ]
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // Everything Should Be Wrapped in a Parent Div
+      <div className="App"> 
+        <h1>Hello, World</h1>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>       
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like Dragon Age</Person>  
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
-}
+};
 
 export default App;
