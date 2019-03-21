8cd2d70801e0ab770273d614cc53f6f693240f46
Caleb Taylor <cteuphonium@gmail.com>
2019-03-21 17:05:34 -0500

    Learned Some Css

--- src/App.js
+++ src/App.js
@@ -6,71 +6,107 @@ class App extends Component {
   // The only things that update the DOM are state (classes) and props (functions)
   state = {
     persons: [
-      { name: 'Caleb', age: '22'},
-      { name: 'Stefani', age: '21'},
-      { name: 'Chester', age: '4'},
+      { id: '0', name: 'Caleb', age: '22'},
+      { id: '1', name: 'Stefani', age: '21'},
+      { id: '2', name: 'Chester', age: '4'},
+      { id: '3', name: 'Olive', age: '1'},
     ]
   }
   //Handler is used to denote an event handler
-  switchNameHandler = (newName) => {
-    // console.log('was clicked')
-    // DONT DO THIS this.state.persons[0].name = "Caleb Taylor";
-    this.setState( {
-      persons: [
-        { name: newName, age: '22'},
-        { name: 'Stefani', age: '21'},
-        { name: 'Chester', age: '5'},
-      ]
-    })
+  switchNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex( p  => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState( {persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Caleb', age: '22'},
-        { name:  event.target.value, age: '21'},
-        { name: 'Chester', age: '5'},
-      ]
-    })
+  // Always try to update state in an immutable way
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons.slice(); // It is better to make a copy of data as apposed to making a pointer
+    // OR const persons = [...this.state.persons]; // It is better to make a copy of data as apposed to making a pointer
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':  {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      }
+    }
+
+
+    // This is checked everytime the DOM is updated
+    let persons = null;
+
+    if (this.state.showPersons){ // Can use if statement outside of return because its not jsx
+      style.backgroundColor = "red";
+      style[':hover'] = {
+        backgroundColor: 'pink',
+        color: 'white',
+      } 
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+                name={person.name}
+                age={person.age}
+                click={this.deletePersonHandler.bind(this, index)}
+                key={ person.id } 
+                changed={ (event) => {
+                  this.switchNameHandler(event, person.id);
+                }}
+                  />
+          })}
+        </div> 
+      )
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    } 
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
     return (
     
       // Everything Should Be Wrapped in a Parent div
 
       // The bind method is a better way then the Arrow function
-
+      
       <div className="App"> 
         <h1>Hello, World</h1>
         <button 
           style ={style}
-          onClick={() => this.switchNameHandler('The Saviour')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          changed = {this.nameChangedHandler}
-          click={this.switchNameHandler.bind(this, "Charlie")}
-          />       
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          changed = {this.nameChangedHandler}
-          click={this.switchNameHandler.bind(this, "Charlie")}>I like Dragon Age</Person>  
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler.bind(this, "Charlie")}
-          changed = {this.nameChangedHandler}
-          />
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          
+        {persons}
+        {/* This is the prefered way to render conditional content */}
+        <p className={classes.join(' ')}>This is really working!</p>
       </div>
+      
+      
     );
   }
 };
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,11 +4,12 @@ import './Person.css';
 // Cannont access state.  Functions are very simple in what they do and that is the benefit.
 // Larger applications should not have many state changing elements.
 const person = (props) => {
+  
     return (
-      <div className="Person">
+      <div className="Person" >
         <p onClick={props.click}>I'm a { props.name } and I am { props.age } years old!</p>
         <p>{ props.children }</p>
-        <input type="text" value={ props.name } onChange={ props.changed } />
+        <input type="text"  onChange={ props.changed } value={ props.name }/>
       </div>
     )
 }
