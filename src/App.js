e550d214a71696245fcc84180c29ec210f8f7af8
Caleb Taylor <cteuphonium@gmail.com>
2019-03-20 20:41:03 -0500

    Learned about event handling and two way binding.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // The only things that update the DOM are state (classes) and props (functions)
   state = {
     persons: [
       { name: 'Caleb', age: '22'},
@@ -10,16 +11,65 @@ class App extends Component {
       { name: 'Chester', age: '4'},
     ]
   }
+  //Handler is used to denote an event handler
+  switchNameHandler = (newName) => {
+    // console.log('was clicked')
+    // DONT DO THIS this.state.persons[0].name = "Caleb Taylor";
+    this.setState( {
+      persons: [
+        { name: newName, age: '22'},
+        { name: 'Stefani', age: '21'},
+        { name: 'Chester', age: '5'},
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Caleb', age: '22'},
+        { name:  event.target.value, age: '21'},
+        { name: 'Chester', age: '5'},
+      ]
+    })
+  }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
-      // Everything Should Be Wrapped in a Parent Div
+    
+      // Everything Should Be Wrapped in a Parent div
+
+      // The bind method is a better way then the Arrow function
+
       <div className="App"> 
         <h1>Hello, World</h1>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>       
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like Dragon Age</Person>  
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button 
+          style ={style}
+          onClick={() => this.switchNameHandler('The Saviour')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          changed = {this.nameChangedHandler}
+          click={this.switchNameHandler.bind(this, "Charlie")}
+          />       
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed = {this.nameChangedHandler}
+          click={this.switchNameHandler.bind(this, "Charlie")}>I like Dragon Age</Person>  
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, "Charlie")}
+          changed = {this.nameChangedHandler}
+          />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Person.css';
 
+// Cannont access state.  Functions are very simple in what they do and that is the benefit.
+// Larger applications should not have many state changing elements.
 const person = (props) => {
     return (
-      <div>
-        <p>I'm a { props.name } and I am { props.age } years old!</p>
+      <div className="Person">
+        <p onClick={props.click}>I'm a { props.name } and I am { props.age } years old!</p>
         <p>{ props.children }</p>
+        <input type="text" value={ props.name } onChange={ props.changed } />
       </div>
     )
 }
